db379429690701d95850c149333db11bcefbded1
FaheemRiaz1 <faheemriaz177@gmail.com>
2021-07-22 18:10:17 +0500

    create or update implementation in personal information + basic information and allergy

--- SBackend/routes/addinfo.js
+++ SBackend/routes/addinfo.js
@@ -154,7 +154,7 @@ router.post('/personalinfo', authenticate.verifyUser, (req, res, next) => {
 
 
 router.post('/allergy', authenticate.verifyUser, (req, res, next) => {
-    Allergy.create({
+    Allergy.updateOne({
         p_id: req.user._id,
         q1: req.body.q1,
         q2: req.body.q2,
@@ -164,11 +164,34 @@ router.post('/allergy', authenticate.verifyUser, (req, res, next) => {
     })
         .then((result) => {
             console.log("Allergy Info has been added", result);
+            if(result.n===0){
+                Allergy.create({
+                    p_id: req.user._id,
+                    q1: req.body.q1,
+                    q2: req.body.q2,
+                    q3: req.body.q3,
+                    q4: req.body.q4,
+                    q5: req.body.q5,
+                })
+                    .then((result) => {
+                        console.log("Allergy Info has been added", result);
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "application/json")
+                        res.json(result)
+                        console.log(result)
+                    }, (err) => next(err)
+            
+                    ).catch((err) => {
+                        res.statusCode = 404;
+                        res.json({ err: err, success: false })
+                    })
+            
+            }else{
             res.statusCode = 200;
             res.setHeader("Content-Type", "application/json")
             res.json(result)
             console.log(result)
-        }, (err) => next(err)
+        }}, (err) => next(err)
 
         ).catch((err) => {
             res.statusCode = 404;
@@ -177,28 +200,50 @@ router.post('/allergy', authenticate.verifyUser, (req, res, next) => {
 
 })
 
-router.post('/background', authenticate.verifyUser, (req, res, next) => {
-    Background.create({
-        p_id: req.user._id,
-        q1: req.body.q1,
-        q2: req.body.q2,
-        q3: req.body.q3,
-        q4: req.body.q4,
-        q5: req.body.q5,
-    })
-        .then((result) => {
-            console.log("Background Info has been added", result);
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json")
-            res.json(result)
-            console.log(result)
-        }, (err) => next(err)
+// router.post('/background', authenticate.verifyUser, (req, res, next) => {
+//     Background.updateOne({
+//         p_id: req.user._id,
+//         q1: req.body.q1,
+//         q2: req.body.q2,
+//         q3: req.body.q3,
+//         q4: req.body.q4,
+//         q5: req.body.q5,
+//     })
+//         .then((result) => {
+//             console.log("Background Info has been added", result);
+//             if(result.n === 0 ){
+//                 Background.create({
+//                     p_id: req.user._id,
+//                     q1: req.body.q1,
+//                     q2: req.body.q2,
+//                     q3: req.body.q3,
+//                     q4: req.body.q4,
+//                     q5: req.body.q5,
+//                 })
+//                     .then((result) => {
+//                         console.log("Background Info has been added", result);
+//                         res.statusCode = 200;
+//                         res.setHeader("Content-Type", "application/json")
+//                         res.json(result)
+//                         console.log(result)
+//                     }, (err) => next(err)
+            
+//                     ).catch((err) => {
+//                         res.statusCode = 404;
+//                         res.json({ err: err, success: false })
+//                     })
+//             }else{
+//             res.statusCode = 200;
+//             res.setHeader("Content-Type", "application/json")
+//             res.json(result)
+//             console.log(result)
+//         }}, (err) => next(err)
 
-        ).catch((err) => {
-            res.statusCode = 404;
-            res.json({ err: err, success: false })
-        })
+//         ).catch((err) => {
+//             res.statusCode = 404;
+//             res.json({ err: err, success: false })
+//         })
 
-})
+// })
 
 module.exports = router;
